666cdbb317ad2163fd94df893a067e3977bd0598
GustavoMedeiros <digital.gustavomedeiros@gmail.com>
2025-04-23 19:36:00 -0300

    Add project files.

---
+++ ContaCorrenteApp.Console/Movimentacao.cs
@@ -0,0 +1,13 @@
+﻿namespace ContaCorrenteApp.Console
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class Movimentacao
+    {
+        
+    }
+}
---
+++ ContaCorrenteApp.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ContaCorrenteApp.Console
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
